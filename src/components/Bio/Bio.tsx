c8aa6600969b3f3e9e960c4db6b69b5ff42fe574
MDixey17 <matthew_dixey@yahoo.com>
2023-06-10 22:30:24 -0500

    profile hover animation

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ProHeadshot from "../../assets/headshot-square.png";
+import Spiderman from "../../assets/spiderman-logo.png";
+import Mandalorian from "../../assets/mando.png";
 import "./Bio.css";
 
 export const Bio = () => {
-  // TODO: let's do a cool hover animation for the headshot
+  const [activeImage, setActiveImage] = useState<string>(ProHeadshot);
+
+  const handleMouseOver = () => {
+    if (Math.random() < 0.5) {
+      setActiveImage(Spiderman);
+    } else {
+      setActiveImage(Mandalorian);
+    }
+  };
+
+  const handleMouseOut = () => {
+    setActiveImage(ProHeadshot);
+  };
+
   return (
     <div className="bio-container">
-      <img src={ProHeadshot} alt="Matthew Dixey" />
+      <img
+        src={activeImage}
+        alt="Matthew Dixey"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      />
       <h1>Matthew Dixey</h1>
       <h2>Software Engineer at Target</h2>
       <p>
